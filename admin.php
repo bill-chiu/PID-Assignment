8e74e3dfd971a11aa560891fc45c38a0daa10396
CS01183 <68837493+bill-chiu@users.noreply.github.com>
2020-08-24 17:37:31 +0800

    管理權限

--- admin.php
+++ admin.php
@@ -41,34 +41,53 @@ $row = mysqli_fetch_assoc($result);
 
         <?php if ($_SESSION["login_session"] == false) { ?>
     <tr>
-      <td>
 
-        <a >This page for user only.</a>
+        <?php if ($_SESSION["id"] == "1") { ?>
+
+          <?php while ($row = mysqli_fetch_assoc($result)) { ?>
+
+      <td><?= $row["username"] ?></td>
+      <td><?= $row["phone"] ?></td>
+      <td><?= $row["account"] ?></td>
+      <td><?= $row["password"] ?></td>
+
+
+   
+        <a href="edit.php?id=<?= $row["studentsId"] ?>" class="btn btn-success btn-sm">Edit</a>
+        <a href="admindelete.php?id=<?= $row["studentsId"] ?>" class="btn btn-danger btn-sm">Delete</a>
       </td>
     </tr>
+  <?php  } ?>
+<?php }  ?>
+<a>This page for user only.</a>
+</td>
+</tr>
 
 
-  <?php } else { ?>
-    <a>您好<?= $_SESSION["user"] ?> </a><br>
-
-    <tr>
-
-
-      <td> 
-        <a href="edit.php?id=<?= $row["userId"] ?>" class="btn btn-success btn-sm">修改帳戶資料</a>
-     
-      </td>
-    </tr>
-  <?php } ?>
+<?php } else { ?>
+  <a>您好<?= $_SESSION["user"] ?> </a><br>
 
   <tr>
-    <td align="left" bgcolor="#CCCCCC"><a href="index.php " class="btn btn-primary  btn-sm">回首頁</a>
+
+
+    <td>
+    <a href="shop_account.php?id=<?= $row["userId"] ?>" class="btn btn-success btn-sm">修改會員資料</a>
+    <a href="shop_car.php?id=<?= $row["userId"] ?>" class="btn btn-success btn-sm">購物車清單</a>
+    <a href="item".php?id=<?= $row["userId"] ?>" class="btn btn-success btn-sm">商品清單</a>
+    <a href="edit.php?id=<?= $row["userId"] ?>" class="btn btn-success btn-sm">修改帳戶資料</a>
+
     </td>
-    <td bgcolor="#CCCCCC"></td>
-    <td bgcolor="#CCCCCC"></td>
-    <td bgcolor="#CCCCCC"></td>
-    <td bgcolor="#CCCCCC"></td>
   </tr>
+<?php } ?>
+
+<tr>
+  <td align="left" bgcolor="#CCCCCC"><a href="index.php " class="btn btn-primary  btn-sm">回首頁</a>
+  </td>
+  <td bgcolor="#CCCCCC"></td>
+  <td bgcolor="#CCCCCC"></td>
+  <td bgcolor="#CCCCCC"></td>
+  <td bgcolor="#CCCCCC"></td>
+</tr>
   </table>
 
 
