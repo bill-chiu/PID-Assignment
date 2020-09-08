06831de0c46336f2b683071f43d6f3f355a07de7
CS01183 <68837493+bill-chiu@users.noreply.github.com>
2020-09-08 17:28:40 +0800

    刪掉管理員多餘頁面

--- shop_account.php
+++ shop_account.php
@@ -73,10 +73,10 @@ multi;
 
             <a><?= $_SESSION["user"] ?>您好</a>
             <a><img src="account_image/<?= $_SESSION['account'] ?>.png" width="40" height="40"></a>
-            <a href="shop_car.php?id=<?= $_SESSION['id'] ?>" class="btn btn-danger   btn-sm">購物車</a>
+
             <a href="sign_out.php" class="btn btn-danger   btn-sm">登出帳號</a>
             <a href="edit.php?id=<?= $_SESSION['id'] ?>" class="btn btn-danger  btn-sm">修改帳號</a>
-            <a href="see_checkout.php?id=<?= $userid ?>" class="btn btn-danger  btn-sm">查看訂單</a>
+
           <?php } ?>
 
 
